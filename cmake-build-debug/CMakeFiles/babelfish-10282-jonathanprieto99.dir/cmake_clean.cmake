f503fff68b398f51eb15f8f4ae92fa37244ecd26
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-06 11:10:43 -0500

    Upload Files

---
+++ cmake-build-debug/CMakeFiles/babelfish-10282-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/babelfish-10282-jonathanprieto99.dir/main.cpp.o"
+  "babelfish-10282-jonathanprieto99"
+  "babelfish-10282-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/babelfish-10282-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/babelfish-10282-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for babelfish-10282-jonathanprieto99.
+# This may be replaced when dependencies are built.
