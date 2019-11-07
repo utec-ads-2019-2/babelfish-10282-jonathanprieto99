2f358f701f0dd9333c4ca95ed79c218b17399e8b
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-06 19:06:57 -0500

    OK

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
-#include <iostream>;
+#include <iostream>
+
+#include <map>
+#include <sstream>
+
 using namespace std;
 
+void ingresar_map(string palabra1, string palabra2, map<string,string> &palabras){
+    palabras[palabra1]=palabra2;
+}
+
+string resultado(map<string,string> &palabras,string buscar){
+    map<string, string>::iterator iterator;
+    iterator = palabras.find(buscar);
+    if(iterator != palabras.end())
+        return iterator->second;
+    else
+        return "eh";
+}
+
 int main(){
+    map<string,string> palabras;
+    string a;
+    string b;
+    string buscar;
+    string linea;
+    while(getline(cin,linea)){
+        if(linea.empty()){
+            break;
+        }
+        istringstream lineanueva(linea);
+        lineanueva>>a>>b;
+        ingresar_map(a,b,palabras);
+    }
+
+    while (cin>>buscar){
+        cout<<resultado(palabras,buscar)<<endl;
+    }
+
 
     return 0;
 }
